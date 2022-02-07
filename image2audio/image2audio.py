1cb84d5f07009d4c47abad8bb8deebbfa172a9b2
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2022-02-07 01:22:59 -0800

    Update image2audio.py

--- image2audio/image2audio.py
+++ image2audio/image2audio.py
@@ -6,3 +6,4 @@ from transformers import (AutoTokenizer, VisionEncoderDecoderModel,
 from fairseq.checkpoint_utils import load_model_ensemble_and_task_from_hf_hub
 from fairseq.models.text_to_speech.hub_interface import TTSHubInterface
 
+if torch.cuda.is_available():
