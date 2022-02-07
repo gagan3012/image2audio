0ffb109efef040dc46d516fec81e1c627c7adab9
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2022-02-07 01:22:56 -0800

    Update image2audio.py

--- image2audio/image2audio.py
+++ image2audio/image2audio.py
@@ -5,3 +5,4 @@ from transformers import (AutoTokenizer, VisionEncoderDecoderModel,
 
 from fairseq.checkpoint_utils import load_model_ensemble_and_task_from_hf_hub
 from fairseq.models.text_to_speech.hub_interface import TTSHubInterface
+
