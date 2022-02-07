490d7d343864984f1d754f36dd98cfe13b4dab0f
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2022-02-07 01:22:49 -0800

    Update image2audio.py

--- image2audio/image2audio.py
+++ image2audio/image2audio.py
@@ -3,3 +3,4 @@ from PIL import Image
 from transformers import (AutoTokenizer, VisionEncoderDecoderModel,
                           ViTFeatureExtractor)
 
+from fairseq.checkpoint_utils import load_model_ensemble_and_task_from_hf_hub
