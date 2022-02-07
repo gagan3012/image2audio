2971bfae97bde11fddd4eb5a8fb9727d5b3a9227
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2022-02-07 01:23:03 -0800

    Update image2audio.py

--- image2audio/image2audio.py
+++ image2audio/image2audio.py
@@ -7,3 +7,4 @@ from fairseq.checkpoint_utils import load_model_ensemble_and_task_from_hf_hub
 from fairseq.models.text_to_speech.hub_interface import TTSHubInterface
 
 if torch.cuda.is_available():
+    device = "cuda"
